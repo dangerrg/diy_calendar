34533b1add58c464390c11c1cb879cc4803f979e
dangerrg <39160873+dangerrg@users.noreply.github.com>
2022-02-07 16:27:07 +0100

    Generate meeting model
    This is for rendering events

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_02_07_152407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "meetings", force: :cascade do |t|
+    t.string "name"
+    t.datetime "start_time", precision: 6
+    t.datetime "end_time", precision: 6
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
