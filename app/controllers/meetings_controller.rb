4dd366058b4e5c1252f9f0029ea48b046791ecc6
dangerrg <39160873+dangerrg@users.noreply.github.com>
2022-02-08 10:02:59 +0100

    Render month_calendar in meeting#index view
    - Add scope to mettings#index action to querry the shown dates

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -4,6 +4,9 @@ class MeetingsController < ApplicationController
   # GET /meetings
   def index
     @meetings = Meeting.all
+    # Scope your query to the dates being shown:
+    start_date = params.fetch(:start_date, Date.today).to_date
+    @meetings = Meeting.where(start_time: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
   end
 
   # GET /meetings/1
